846c2fb0ee60c39d53abc26f6e1c0295b0e18241
leannnica <laloisi@fi.uba.ar>
2025-07-18 23:55:03 -0300

    Cambios en el 'layout' para implementacion del DARKMODE. Se utiliza ThemeProvider de la libreria next-themes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
 
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+export const metadata = {
+  title: "Mi rutina de gimnasio",
+  description: "App de entrenamiento",
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
